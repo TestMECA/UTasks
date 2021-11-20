d61a64bb18ed35792dcd43f6dc0c08a5de8c5aeb
MohamedRaslan <mohamedraslang@gmail.com>
2021-11-20 17:36:42 +0200

    Initial project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+export const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        UTasks is a simple <a href="https://todoist.com/">Todoist </a> clone
+      </p>
+    </header>
+  </div >
+);
