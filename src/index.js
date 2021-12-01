178852d2147d6b220c26e71dfd6b6c011d3f3d1c
MohamedRaslan <mohamedraslang@gmail.com>
2021-12-01 02:15:16 +0200

    remove part of the console info

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import { NODE_ENV, BUILD_TARGET, BUILD_NAME, APP_VERSION } from "src/config/cons
 console.info(`
   TARGET: [ ${BUILD_TARGET} ] on [ ${NODE_ENV} ] environment
   FOR     ${BUILD_NAME}@V${APP_VERSION}
-  Project: ${require('./utasks-configuration.json').result.sdkConfig.projectId}
   `);
 
 
