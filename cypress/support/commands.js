a59f9801c8ba75ff8e687d64a0f1b9d2ce630a47
MohamedRaslan <mohamedraslang@gmail.com>
2021-12-18 03:39:59 +0200

    fix github workflows

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,76 +25,36 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 import 'cypress-file-upload';
 
-Cypress.Commands.add('getAccessToken', (userCredentials = {}) => {
-  const COGNITO_API = Cypress.env('APIS').COGNITO;
-  const DEFAULT_ACCOUNT = Cypress.env('TESTING_ACCOUNTS').DOCTOR_USER_1;
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
+
+
+import { signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { auth } from "../../src/firebase"
+
+
+Cypress.Commands.add('login', (userCredentials = {}) => {
+  const DEFAULT_ACCOUNT = Cypress.env('TESTING_ACCOUNTS').UTASKS_USER1;
 
   if (!userCredentials || !userCredentials.userName)
     userCredentials.userName = DEFAULT_ACCOUNT.USERNAME;
   if (!userCredentials || !userCredentials.password)
     userCredentials.password = DEFAULT_ACCOUNT.PASSWORD;
 
-  return cy.request({
-    method: 'POST',
-    url: `https://${COGNITO_API.HOST_NAME}/`,
-    headers: {
-      'Content-Type': 'application/x-amz-json-1.1',
-      'X-Amz-Target': 'AWSCognitoIdentityProviderService.InitiateAuth',
-    },
-    body: {
-      AuthParameters: {
-        USERNAME: userCredentials.userName,
-        PASSWORD: userCredentials.password,
-      },
-      AuthFlow: COGNITO_API.AUTH_FLOW,
-      ClientId: COGNITO_API.CLIENT_ID,
-    },
+  signInWithEmailAndPassword(auth, userCredentials.userName, userCredentials.password).then(() => {
+    cy.log("Signed in  successful.")
+  }).catch(e => {
+    cy.log("Failed to sign in - ", e)
   });
+
 });
 
-Cypress.Commands.add('changePassword', (userCredentials = {}) => {
-  const COGNITO_API = Cypress.env('APIS').COGNITO;
-  const DEFAULT_ACCOUNT = Cypress.env('TESTING_ACCOUNTS').DOCTOR_USER_1;
+Cypress.Commands.add('logout', () => {
+  signOut(auth).then(() => {
+    console.log("Sign-out successful.")
+  }).catch(e => {
+    console.log("Failed to logout - ", e)
+  });
 
-  if (!userCredentials.previousPassword)
-    userCredentials.previousPassword = DEFAULT_ACCOUNT.PASSWORD;
-  if (!userCredentials.proposedPassword)
-    userCredentials.proposedPassword = DEFAULT_ACCOUNT.PASSWORD;
-
-  //If you didn't provide a access token , i will get it form the cookies
-  if (!userCredentials.accessToken)
-    cy.getCookie('at')
-      .should('exist')
-      .then((cookie) => {
-        userCredentials.accessToken = cookie.value;
-        cy.request({
-          method: 'POST',
-          url: `https://${COGNITO_API.HOST_NAME}/`,
-          headers: {
-            'Content-Type': 'application/x-amz-json-1.1',
-            'X-Amz-Target': 'AWSCognitoIdentityProviderService.ChangePassword',
-          },
-          body: {
-            AccessToken: userCredentials.accessToken,
-            PreviousPassword: userCredentials.previousPassword,
-            ProposedPassword: userCredentials.proposedPassword,
-          },
-        });
-      });
-  else
-    cy.request({
-      method: 'POST',
-      url: `https://${COGNITO_API.HOST_NAME}/`,
-      headers: {
-        'Content-Type': 'application/x-amz-json-1.1',
-        'X-Amz-Target': 'AWSCognitoIdentityProviderService.ChangePassword',
-      },
-      body: {
-        AccessToken: userCredentials.accessToken,
-        PreviousPassword: userCredentials.previousPassword,
-        ProposedPassword: userCredentials.proposedPassword,
-      },
-    });
 });
 
 
