697af984ba51ef517456fca8215b7fc7d4f78111
MohamedRaslan <mohamedraslang@gmail.com>
2021-12-25 07:39:41 +0200

    fix configs, update some pakages and some tests

--- cypress/integration/miscellaneous/utasks-performance.spec.js
+++ cypress/integration/miscellaneous/utasks-performance.spec.js
@@ -1,7 +1,7 @@
+/* eslint-disable spellcheck/spell-checker */
 /// <reference types="cypress"/>
 
-const UTASKS_URL = Cypress.env('PORTALS_URL')[Cypress.env('TESTING_ENV')];
-import 'firebase/compat/auth';
+const UTASKS_URL = Cypress.env('PORTALS_URL')[Cypress.env('TESTING_URL')];
 
 describe('UTAsKS - Client Performance', () => {
   before(() => {
@@ -9,16 +9,30 @@ describe('UTAsKS - Client Performance', () => {
   });
 
   context('Checking utasks pages performance', () => {
-    beforeEach(() => {
-      cy.visit(UTASKS_URL.UTASKS);
-    });
-
     it('Test home', () => {
       cy.visit(`${UTASKS_URL.UTASKS}home`);
+      cy.get('[data-testid=tasks]').should('exist');
     });
 
     it('Test home2', () => {
       cy.visit(`${UTASKS_URL.UTASKS}home`);
+      cy.get('[data-testid=tasks]').should('exist');
+    });
+
+    it('Test home3', () => {
+      cy.visit(`${UTASKS_URL.UTASKS}update-profile`);
+      cy.get('[data-testid=update-card]').should('exist');
+    });
+
+    it('Test home4', () => {
+      cy.visit(`${UTASKS_URL.UTASKS}`);
+      cy.get('[data-testid=profile]').should('exist');
+      cy.logout();
+    });
+
+    it('Test home5', () => {
+      cy.visit(`${UTASKS_URL.UTASKS}`);
+      cy.get('[data-testid=login-card]').should('exist');
     });
   }); // End of Context - Checking utasks pages performance
 }); // End of Describe - UTAsKS - Client Performance
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,103 +1,102 @@
 import React, { useState } from 'react';
-import { FaLightbulb, FaRegLightbulb } from 'react-icons/fa'
+import { FaLightbulb, FaRegLightbulb } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 import { AddTask } from '../AddTask';
-import { useAuth } from "../../context/AuthContext"
-
+import { useAuth } from '../../context/AuthContext';
 
 export const Header = ({ darkMode, setDarkMode }) => {
-    const [shouldShowMain, setShouldShowMain] = useState(false);
-    const [showQuickAddTask, setShowQuickAddTask] = useState(false);
-    const { logout } = useAuth()
+  const [shouldShowMain, setShouldShowMain] = useState(false);
+  const [showQuickAddTask, setShowQuickAddTask] = useState(false);
+  const { logout } = useAuth();
 
-
-    async function handleLogout() {
-
-        try {
-            await logout()
-            history.push("/login")
-        } catch {
-            console.log("Failed to log out")
-        }
+  async function handleLogout() {
+    try {
+      await logout();
+      history.push('/login');
+    } catch {
+      console.log('Failed to log out');
     }
+  }
 
+  return (
+    <header className="header" data-testid="header">
+      <nav>
+        <div className="logo">
+          {!darkMode ? (
+            <img src="/images/logo.svg" alt="UTasks" />
+          ) : (
+            <p> Deez Nuts!!👀 </p>
+          )}
+        </div>
+        <div className="settings">
+          <ul>
+            {!darkMode ? (
+              <li className="settings__add">
+                <button
+                  data-testid="quick-add-task-action"
+                  aria-label="Quick add task"
+                  type="button"
+                  onClick={() => {
+                    setShowQuickAddTask(true);
+                    setShouldShowMain(true);
+                  }}
+                >
+                  +
+                </button>
+              </li>
+            ) : (
+              <li className="settings__add">
+                <button
+                  data-testid="quick-add-task-action"
+                  aria-label="Quick add task"
+                  type="button"
+                  onClick={() => {
+                    setShowQuickAddTask(true);
+                    setShouldShowMain(true);
+                  }}
+                >
+                  🐧
+                </button>
+              </li>
+            )}
+            <li className="settings__darkmode">
+              <button
+                data-testid="dark-mode-action"
+                aria-label="Darkmode on/off"
+                type="button"
+                onClick={() => setDarkMode(!darkMode)}
+              >
+                {darkMode ? <FaLightbulb /> : <FaRegLightbulb />}
+              </button>
+            </li>
 
-    return (
-        <header className="header" data-testid="header">
-            <nav>
-                <div className="logo">
-                    {!darkMode ? <img src="/images/logo.svg" alt="UTasks" /> : <p> Deez Nuts!!👀 </p>}
-                </div>
-                <div className="settings">
-                    <ul>
-                        {!darkMode ?
-                            <li className="settings__add">
-                                <button
-                                    data-testid="quick-add-task-action"
-                                    aria-label="Quick add task"
-                                    type="button"
-                                    onClick={() => {
-                                        setShowQuickAddTask(true);
-                                        setShouldShowMain(true);
-                                    }}
-                                >
-                                    +
-                                </button>
-                            </li>
-                            : <li className="settings__add">
-                                <button
-                                    data-testid="quick-add-task-action"
-                                    aria-label="Quick add task"
-                                    type="button"
-                                    onClick={() => {
-                                        setShowQuickAddTask(true);
-                                        setShouldShowMain(true);
-                                    }}
-                                >
-                                    🐧
-                                </button>
-                            </li>
-                        }
-                        <li className="settings__darkmode">
-                            <button
-                                data-testid="dark-mode-action"
-                                aria-label="Darkmode on/off"
-                                type="button"
-                                onClick={() => setDarkMode(!darkMode)}
-                            >
-                                {darkMode ? (<FaLightbulb />) : (<FaRegLightbulb />)}
-                            </button>
-                        </li>
+            <li className="settings__darkmode">
+              <button
+                data-testid="log-out-action"
+                aria-label="Log out"
+                type="button"
+                onClick={handleLogout}
+              >
+                {!darkMode && 'Log Out'}
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
 
-                        <li className="settings__darkmode">
-                            <button
-                                data-testid="log-out-action"
-                                aria-label="Log out"
-                                type="button"
-                                onClick={handleLogout}
-                            >
-                                {!darkMode && "Log Out"}
-                            </button>
-                        </li>
-                    </ul>
-
-                </div>
-
-            </nav>
-
-            {!darkMode &&
-                <AddTask
-                    showAddTaskMain={false}
-                    shouldShowMain={shouldShowMain}
-                    showQuickAddTask={showQuickAddTask}
-                    setShowQuickAddTask={setShowQuickAddTask}
-                />
-            }
-        </header>
-    )
+      {!darkMode && (
+        <AddTask
+          showAddTaskMain={false}
+          shouldShowMain={shouldShowMain}
+          showQuickAddTask={showQuickAddTask}
+          setShowQuickAddTask={setShowQuickAddTask}
+        />
+      )}
+    </header>
+  );
 };
 
 Header.propTypes = {
-    darkMode: PropTypes.bool.isRequired,
-    setDarkMode: PropTypes.func.isRequired,
+  darkMode: PropTypes.bool.isRequired,
+  setDarkMode: PropTypes.func.isRequired,
 };
